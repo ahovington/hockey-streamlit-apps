11341719b183567789da6086a103ebe3fe88c6b8
ahovington <10417538+ahovington@users.noreply.github.com>
2023-12-30 18:21:16 +1100

    fix auth

--- selections/auth.py
+++ selections/auth.py
@@ -7,21 +7,6 @@ import streamlit_authenticator as stauth
 with open("selections/config.yaml") as file:
     config = yaml.load(file, Loader=SafeLoader)
 
-authenticator = stauth.Authenticate(
-    credentials=config["credentials"],
-    cookie_name=config["cookie"]["name"],
-    key=config["cookie"]["key"],
-    cookie_expiry_days=config["cookie"]["expiry_days"],
-)
-
-# TODO: write validator with the following methods
-# if not self.validator.validate_username(username):
-#     raise RegisterError('Username is not valid')
-# if not self.validator.validate_name(name):
-#     raise RegisterError('Name is not valid')
-# if not self.validator.validate_email(email):
-#     raise RegisterError('Email is not valid')
-
 
 def login() -> bool:
     """Login into app
@@ -29,12 +14,13 @@ def login() -> bool:
     Returns:
         bool: The result of the login attempt, True if successful.
     """
-    _ = authenticator.login("Login", "main")
+    authenticator = _authenticator()
+    authenticator.login("Login", "main")
     if st.session_state["authentication_status"]:
         col1, _ = st.columns([1, 5])
         authenticator.logout("Logout", "main", key="unique_key")
         if col1.button("Reset password"):
-            _reset_password()
+            _reset_password(authenticator)
         return True
     elif st.session_state["authentication_status"] is False:
         st.error("Username/password is incorrect")
@@ -44,11 +30,11 @@ def login() -> bool:
         return False
 
 
-def _reset_password():
+def _reset_password(authenticator: stauth.Authenticate):
     try:
         if authenticator.reset_password(st.session_state["username"], "Reset password"):
             st.success("Password modified successfully")
-            update_config()
+            _update_config()
     except Exception as e:
         st.error(e)
 
@@ -57,11 +43,20 @@ def register_user():
     try:
         if authenticator.register_user("Create user", preauthorization=False):
             st.success("User registered successfully")
-            update_config()
+            _update_config()
     except Exception as e:
         st.error(e)
 
 
-def update_config():
+def _update_config():
     with open("selections/config.yaml", "w") as file:
         yaml.dump(config, file, default_flow_style=False)
+
+
+def _authenticator() -> stauth.Authenticate:
+    return stauth.Authenticate(
+        credentials=config["credentials"],
+        cookie_name=config["cookie"]["name"],
+        key=config["cookie"]["key"],
+        cookie_expiry_days=config["cookie"]["expiry_days"],
+    )
