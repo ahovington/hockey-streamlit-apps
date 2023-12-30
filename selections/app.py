fa68f17257c7157243bf70f23768819c5fcc6741
ahovington <10417538+ahovington@users.noreply.github.com>
2023-12-30 13:31:19 +1100

    add reset password and register user

--- selections/app.py
+++ selections/app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from auth import login_page
+from auth import login, register_user
 from documentation import Documentation
 from selections import Selections
 from grade_assignment import GradeAssignment
@@ -22,10 +22,14 @@ if __name__ == "__main__":
     with st.sidebar:
         database_lock = st.toggle("Lock database", True)
         season = st.selectbox("Season", ["2023", "2024"])
+
     col1, col2 = st.columns([2, 3])
     col1.image("./rosella.png")
-    app_name = tuple(apps.keys())[0]
-    app_name = col2.selectbox("Select page", tuple(apps.keys()))
 
-    if login_page():
+    if login():
+        app_name = tuple(apps.keys())[0]
+        app_name = col2.selectbox("Select page", tuple(apps.keys()))
         apps[app_name](database_lock, season)
+    else:
+        with st.expander("Create login"):
+            register_user()
--- selections/auth.py
+++ selections/auth.py
@@ -3,26 +3,47 @@ from yaml.loader import SafeLoader
 import streamlit as st
 import streamlit_authenticator as stauth
 
-# st.write(stauth.Hasher(["admin"]).generate())
 
 with open("selections/config.yaml") as file:
     config = yaml.load(file, Loader=SafeLoader)
 
+authenticator = stauth.Authenticate(
+    config["credentials"], config["cookie"]["name"], config["cookie"]["key"]
+)
 
-def login_page() -> bool:
-    authenticator = stauth.Authenticate(
-        config["credentials"], config["cookie"]["name"], config["cookie"]["key"]
-    )
 
-    name, authentication_status, username = authenticator.login("Login", "main")
+def login() -> bool:
+    """Login into app
 
-    if authentication_status:
-        authenticator.logout("Logout", "main")
-        st.write(f"Welcome *{name}*")
+    Returns:
+        bool: The result of the login attempt, True if successful.
+    """
+    authenticator.login("Login", "main")
+    if st.session_state["authentication_status"]:
+        col1, _ = st.columns([1, 5])
+        authenticator.logout("Logout", "main", key="unique_key")
+        if col1.button("Reset password"):
+            _reset_password()
         return True
-    elif authentication_status == False:
+    elif st.session_state["authentication_status"] is False:
         st.error("Username/password is incorrect")
         return False
-    elif authentication_status == None:
+    elif st.session_state["authentication_status"] is None:
         st.warning("Please enter your username and password")
         return False
+
+
+def _reset_password():
+    try:
+        if authenticator.reset_password(st.session_state["username"], "Reset password"):
+            st.success("Password modified successfully")
+    except Exception as e:
+        st.error(e)
+
+
+def register_user():
+    try:
+        if authenticator.register_user("Create user", preauthorization=False):
+            st.success("User registered successfully")
+    except Exception as e:
+        st.error(e)
