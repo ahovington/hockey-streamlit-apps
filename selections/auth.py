f8f0298b1f0269192e62fa82d724b1bb45f2f6a9
ahovington <10417538+ahovington@users.noreply.github.com>
2023-12-31 15:24:43 +1100

    refactor auth

--- selections/auth.py
+++ selections/auth.py
@@ -4,10 +4,6 @@ import streamlit as st
 import streamlit_authenticator as stauth
 
 
-with open("selections/config.yaml") as file:
-    config = yaml.load(file, Loader=SafeLoader)
-
-
 def login() -> bool:
     """Login into app
 
@@ -17,44 +13,67 @@ def login() -> bool:
     authenticator = _authenticator()
     authenticator.login("Login", "main")
     if st.session_state["authentication_status"]:
-        col1, _ = st.columns([1, 5])
         authenticator.logout("Logout", "main", key="unique_key")
-        if col1.button("Reset password"):
-            _reset_password(authenticator)
+        _reset_password(authenticator)
         return True
-    elif st.session_state["authentication_status"] is False:
+    if st.session_state["authentication_status"] is False:
         st.error("Username/password is incorrect")
         return False
-    elif st.session_state["authentication_status"] is None:
+    if st.session_state["authentication_status"] is None:
         st.warning("Please enter your username and password")
         return False
 
 
-def _reset_password(authenticator: stauth.Authenticate):
-    try:
-        if authenticator.reset_password(st.session_state["username"], "Reset password"):
-            st.success("Password modified successfully")
-            _update_config()
-    except Exception as e:
-        st.error(e)
+def _reset_password(authenticator: stauth.Authenticate) -> None:
+    """Resets users password after login
+
+    Args:
+        authenticator (stauth.Authenticate): The authenticator used to login.
+    """
+    if authenticator.reset_password(
+        username=st.session_state["username"],
+        form_name="Reset password",
+        location="sidebar",
+    ):
+        st.success("Password modified successfully")
+        _update_config(authenticator)
 
 
-def register_user():
+def register_user() -> None:
+    """Register a new user."""
     authenticator = _authenticator()
-    try:
-        if authenticator.register_user("Create user", preauthorization=False):
-            st.success("User registered successfully")
-            _update_config()
-    except Exception as e:
-        st.error(e)
+    if authenticator.register_user("Create user", preauthorization=False):
+        st.success("User registered successfully")
+        _update_config(authenticator)
 
 
-def _update_config():
-    with open("selections/config.yaml", "w") as file:
+def _update_config(authenticator: stauth.Authenticate) -> None:
+    """Update the users login details.
+
+    Args:
+        authenticator (stauth.Authenticate): The authenticator used to login.
+    """
+    config = {
+        "credentials": authenticator.credentials,
+        "cookie": {
+            "expiry_days": authenticator.cookie_expiry_days,
+            "key": authenticator.key,
+            "name": authenticator.cookie_name,
+        },
+        "preauthorized": authenticator.preauthorized,
+    }
+    with open("selections/config.yaml", "w", encoding="utf-8") as file:
         yaml.dump(config, file, default_flow_style=False)
 
 
 def _authenticator() -> stauth.Authenticate:
+    """Pass the paramaters to the authenicator.
+
+    Returns:
+        stauth.Authenticate: The authenicator.
+    """
+    with open("selections/config.yaml", "r", encoding="utf-8") as file:
+        config = yaml.load(file, Loader=SafeLoader)
     return stauth.Authenticate(
         credentials=config["credentials"],
         cookie_name=config["cookie"]["name"],
