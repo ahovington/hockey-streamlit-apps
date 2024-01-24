ebc784d4be4c5282874834f6a8e5d43fd0f64d49
ahovington <10417538+ahovington@users.noreply.github.com>
2024-01-24 17:41:47 +1100

    add table summarising largest debtors

--- apps/finance/club_fees_outstanding.py
+++ apps/finance/club_fees_outstanding.py
@@ -21,9 +21,11 @@ def ClubFeesOustanding() -> None:
     overdue = _invoices[_invoices["due_date"] <= dt.datetime.now()]
     col1, col2, col3, col4 = st.columns([1, 1, 1, 1])
     col1.metric(
-        "Net amount", financial_string_formatting(overdue["total_amount"].sum())
+        "Net amount", financial_string_formatting(overdue["total_amount_due"].sum())
     )
-    col2.metric("Gross amount", financial_string_formatting(overdue["amount"].sum()))
+    col2.metric(
+        "Gross amount", financial_string_formatting(overdue["amount_invoiced"].sum())
+    )
     col3.metric(
         "Discounts applied", financial_string_formatting(overdue["discount"].sum())
     )
@@ -34,13 +36,17 @@ def ClubFeesOustanding() -> None:
 
     # Table of overdue fees
     st.subheader("OVERDUE CLUB FEES", divider="green")
+    st.write("OVERDUE INVOICES", divider="green")
     st.dataframe(overdue, hide_index=True, use_container_width=True)
+    st.write("PLAYERS WITH MULTIPLE INVOICES", divider="green")
+    st.dataframe(largest_over_due_debitors(), hide_index=True, use_container_width=True)
 
     # Table of fees due in the future
     st.subheader("UPCOMING CLUB FEES", divider="green")
     upcoming = _invoices[_invoices["due_date"] > dt.datetime.now()]
     st.metric(
-        "Upcoming fees due", financial_string_formatting(upcoming["total_amount"].sum())
+        "Upcoming fees due",
+        financial_string_formatting(upcoming["total_amount_due"].sum()),
     )
     st.dataframe(upcoming, hide_index=True, use_container_width=True)
 
@@ -57,11 +63,11 @@ def invoice_data() -> pd.DataFrame:
             p.full_name,
             r.grade,
             i.due_date,
-            i.amount,
+            i.amount as amount_invoiced,
             i.discount,
             i.amount_paid,
             i.per_game_adjustment_applied as per_game_adjustment,
-            i.amount - i.discount - i.amount_credited as total_amount,
+            i.amount - i.discount - i.amount_credited - i.amount_paid as total_amount_due,
             i.on_payment_plan
         from invoices as i
         inner join registrations as r
@@ -74,7 +80,53 @@ def invoice_data() -> pd.DataFrame:
         order by
             r.season,
             i.due_date,
-            total_amount desc,
+            total_amount_due desc,
             issued_date asc
+    """
+    )
+
+
+def largest_over_due_debitors() -> pd.DataFrame:
+    """Extact the outstanding club fees.
+
+    Retuns:
+        pd.DataFrame: The results of the query.
+    """
+    return read_data(
+        f"""
+        with outstanding_invoices as (
+            select
+                p.id as player_id,
+                count(*)
+            from invoices as i
+            inner join players as p
+            on i.player_id = p.id
+            where
+                i.status not in ('PAID', 'VOID', 'VOIDED', 'DELETED')
+            group by
+                p.id
+            having
+                count(*) > 1
+        )
+
+        select
+            p.full_name,
+            sum(i.amount) as amount_invoiced,
+            sum(i.discount) as discount,
+            sum(i.amount_paid) as amount_paid,
+            sum(i.per_game_adjustment_applied) as per_game_adjustment,
+            sum(i.amount - i.discount - i.amount_credited) as total_amount_due,
+            max(i.on_payment_plan::int)::boolean as on_payment_plan
+        from invoices as i
+        inner join registrations as r
+        on i.registration_id = r.id
+        inner join players as p
+        on i.player_id = p.id
+        inner join outstanding_invoices as oi
+        on i.player_id = oi.player_id
+        group by
+            p.full_name
+        order by
+            total_amount_due desc
     """
     )
