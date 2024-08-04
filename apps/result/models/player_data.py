808519574d13e35b3c991ff6aedeb81869ff82f8
ahovington <10417538+ahovington@users.noreply.github.com>
2024-08-04 16:58:05 +1000

    refactor and improve the player profile

--- apps/result/models/player_data.py
+++ apps/result/models/player_data.py
@@ -3,52 +3,109 @@ import pandas as pd
 from utils import read_data
 
 
-def player_data(player_name: str, season: str) -> pd.DataFrame:
+def player_data(player_id: str, season: str) -> pd.DataFrame:
     """The data for the player
 
     Args:
-        player_name str: The name of the player.
+        player_id str: The id of the player.
         season str: The hockey season.
     """
+    df = read_data(
+        f"""
+       select
+            p.id as player_id,
+            p.full_name as player,
+            g.id as game_id,
+            t.team,
+            r.grade as player_graded,
+            t.grade,
+            s.goal_keeper,
+            s.played,
+            g.season,
+            g.round,
+            case
+                when l.name = 'Newcastle International Hockey Centre'
+                then 'NIHC'
+                else l.name
+            end as location_name,
+            g.finals,
+            case
+                when g.opposition = '4thGreen' then 'West Green'
+                when g.opposition = '4thRed' then 'West Red'
+                else g.opposition 
+            end as opposition,
+            g.start_ts,
+            g.goals_for,
+            g.goals_against,
+            case
+                when g.goals_for > g.goals_against then 'win'
+                when g.goals_for < g.goals_against then 'loss'
+                when g.goals_for = g.goals_against then 'draw'
+            end as result,
+            coalesce(re.goals, 0) as goals,
+            coalesce(re.green_card, 0) as green_card,
+            coalesce(re.yellow_card, 0) as yellow_card,
+            coalesce(re.red_card, 0) as red_card,
+            coalesce(re.green_card, 0) + coalesce(re.yellow_card, 0) + coalesce(re.red_card, 0) as cards
+        from players as p
+        inner join registrations as r
+        on p.id = r.player_id
+        inner join selections as s
+        on p.id = s.player_id
+        left join results as re
+        on s.id = re.id
+        inner join games as g
+        on s.game_id = g.id and
+            r.season = g.season
+        inner join teams as t
+        on g.team_id = t.id
+         inner join locations as l
+        on g.location_id = l.id
+        where
+            s.played = true and
+            p.id = '{player_id}' and
+            r.season = '{season}'
+        """
+    )
+    df.loc[:, "goals_for"] = (
+        df.loc[:, "goals_for"].replace("", 0).astype(float).astype(int)
+    )
+    df.loc[:, "goals_against"] = (
+        df.loc[:, "goals_against"].replace("", 0).astype(float).astype(int)
+    )
+    return df
+
+
+def player_names(season: str) -> pd.DataFrame:
     return read_data(
         f"""
+        with _goals as (
             select
-                p.id as player_id,
-                p.full_name as player,
-                g.season,
-                r.grade as player_graded,
-                t.grade as grade_played,
-                s.goal_keeper,
-                s.played,
-                count(*) as games_played
-            from players as p
-            left join registrations as r
-            on p.id = r.player_id
-            left join selections as s
-            on p.id = s.player_id
-            left join games as g
-            on s.game_id = g.id and
-                r.season = g.season
-            left join teams as t
-            on g.team_id = t.id
+                s.player_id,
+                sum(r.goals) as goals
+
+            from games as g
+            inner join selections as s
+            on g.id = s.game_id
+            inner join results as r
+            on s.id = r.id
             where
                 s.played = true and
-                p.full_name = '{ player_name }' and
-                r.season = '{ season }' and
-                g.season = '{ season }'
+                g.season = '{season}'
             group by
-                p.id,
-                p.full_name,
-                g.season,
-                r.grade,
-                t.grade,
-                s.goal_keeper,
-                s.played
-            order by
-                p.id
-    """
+                s.player_id
+        )
+
+        select
+            p.id,
+            p.full_name as player
+        from registrations as r
+        inner join players as p
+        on r.player_id = p.id
+        left join _goals as g
+        on p.id = g.player_id
+        where r.season = '{season}'
+        order by
+            coalesce(g.goals, 0) desc
+        """
     )
-
-
-def player_names() -> pd.DataFrame:
-    return read_data("select full_name as player from players order by full_name")
--- apps/result/player_profile.py
+++ apps/result/player_profile.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 import streamlit as st
 
 from config import config
@@ -5,34 +6,125 @@ from utils import select_box_query
 from result.models import player_data, player_names
 
 
+@dataclass
+class Player:
+    name: str
+    grade: str
+    games_played: int
+    games_won: int
+    goals: int
+    green_cards: int
+    yellow_cards: int
+    red_cards: int
+
+    @property
+    def total_cards(self) -> int:
+        return self.green_cards + self.yellow_cards + self.red_cards
+
+    @property
+    def goals_per_game(self) -> float:
+        if self.goals == 0 or self.games_played == 0:
+            return 0.0
+        return self.goals / self.games_played
+
+    @property
+    def percent_games_won(self) -> str:
+        return f"{(self.games_won / self.games_played) * 100:.1f}%"
+
+
 def main() -> None:
     """Display player results"""
 
-    _, col2, col3, col4, _ = st.columns([3, 2, 2, 2, 1], gap="small")
+    col1, col2, _, _ = st.columns(
+        [2, 2, 2, 2], gap="small", vertical_alignment="center"
+    )
+    season = select_box_query(
+        "Season",
+        config.app.seasons,
+        col1,
+        "Select season...",
+    )
+    if not season:
+        # Seasons are stored in ascending order
+        season = config.app.seasons[::-1][0]
+
+    players = player_names(season)
     player_name = select_box_query(
         "Player",
-        player_names()["player"].values.tolist(),
+        players["player"].values.tolist(),
         col2,
         "Select player...",
     )
-    season = select_box_query("Season", config.app.seasons, col3, "Select season...")
+    if not player_name:
+        # By default display player with the most goals
+        player_name = players["player"].values.tolist()[0]
+    player_id = players[players["player"] == player_name]["id"].values[0]
+    st.subheader("", divider="green")
 
-    if not player_name or not season:
-        st.warning("Pick a player and season from the dropdowns.")
-        return
-    st.write("")
-    st.write("")
-
-    df = player_data(player_name, season)
-
+    df = player_data(player_id, season)
     if not df.shape[0]:
+        st.warning(f"No games found for {player_name} in the {season} season")
+        return
+
+    player = Player(
+        name=player_name,
+        grade=df["player_graded"].unique()[0],
+        games_played=int(df.shape[0]),
+        games_won=df[df["result"] == "win"].shape[0],
+        goals=int(df["goals"].sum()),
+        green_cards=int(df["green_card"].sum()),
+        yellow_cards=int(df["yellow_card"].sum()),
+        red_cards=int(df["red_card"].sum()),
+    )
+
+    if not player.games_played:
         st.warning(f"{ player_name } hasn't played any games in the { season } season.")
         return
 
-    grade = df["player_graded"].unique()[0]
-    games_played = df["games_played"].sum()
+    display_html_header(player.name, season, player.grade)
 
-    st.markdown(
+    col1, col2, col3, col4, col5 = st.columns(
+        [1, 1, 1, 1, 1], gap="small", vertical_alignment="center"
+    )
+    col1.metric("Games played", player.games_played)
+    col2.metric("Games won %", player.percent_games_won)
+    col3.metric("Goals", player.goals)
+    col4.metric("Goals per game", player.goals_per_game)
+    col5.metric("Cards", player.total_cards)
+
+    player_table = df[
+        [
+            "team",
+            "grade",
+            "round",
+            "location_name",
+            "opposition",
+            "goals",
+            "cards",
+            "goals_for",
+            "goals_against",
+            "result",
+        ]
+    ]
+
+    st.subheader("Games played")
+    player_table.columns = [
+        "Team",
+        "Grade",
+        "Round",
+        "Location",
+        "Opposition",
+        "Individual Goals",
+        "Individual Cards",
+        "Team Goals For",
+        "Team Goals Against",
+        "Result",
+    ]
+    st.dataframe(player_table, hide_index=True, use_container_width=True)
+
+
+def display_html_header(player_name: str, season: str, grade: str):
+    st.html(
         f"""
         <div class="profile">
             <style>
@@ -53,21 +145,10 @@ def main() -> None:
         </style>
             <img src= { "https://maxfieldhockey.com/wp-content/uploads/2019/02/PlayerProfileHeadshot_Default.jpg" } alt="Player Avatar">
             <h1>{ player_name }</h1>
-            <p>Player Graded: { grade }</p>
-            <p>Games played in { season }: { games_played }</p>
+            <p>{season} Season : Player Graded: { grade }</p>
         </div>
-        """,
-        unsafe_allow_html=True,
+        """
     )
-
-    player_table = df[["grade_played", "goal_keeper", "games_played"]]
-    player_table.columns = [
-        "Grade played",
-        "Played in goals",
-        "Games played",
-    ]
-    player_table.loc[:, "Goals"] = 0
-    st.dataframe(player_table, hide_index=True, use_container_width=True)
 
 
 main()
